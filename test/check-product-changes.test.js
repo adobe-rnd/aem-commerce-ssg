7e7eca02266fdafc4d4332e7f23df6e3b9d42f7b
Duy Nguyen <duy.phnguyen@gmail.com>
2025-03-02 16:58:00 +0100

    clean up imports

--- test/check-product-changes.test.js
+++ test/check-product-changes.test.js
@@ -11,11 +11,11 @@ governing permissions and limitations under the License.
 */
 
 const assert = require('node:assert/strict');
-const { loadState, saveState, getStateFileLocation, poll } = require('../actions/check-product-changes/poller.js');
-const Files = require('./__mocks__/files.js');
-const { AdminAPI } = require('../actions/lib/aem.js');
+const { loadState, saveState, getStateFileLocation, poll } = require('../actions/check-product-changes/poller');
+const Files = require('./__mocks__/files');
+const { AdminAPI } = require('../actions/lib/aem');
 const { requestSaaS, requestSpreadsheet, isValidUrl} = require('../actions/utils');
-const { MockState } = require('./__mocks__/state.js');
+const { MockState } = require('./__mocks__/state');
 
 const EXAMPLE_STATE = 'sku1,1,\nsku2,2,\nsku3,3,';
 
