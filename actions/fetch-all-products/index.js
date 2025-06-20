a37b2ed10074b1a98bf9e938420fadc41bc72314
Dirk Rudolph <drudolph@adobe.com>
2025-06-20 14:33:20 +0200

    Update index.js

--- actions/fetch-all-products/index.js
+++ actions/fetch-all-products/index.js
@@ -14,7 +14,7 @@ governing permissions and limitations under the License.
 
 const { CategoriesQuery, ProductCountQuery, ProductsQuery } = require('../queries');
 const { Core, Files } = require('@adobe/aio-sdk')
-const { requestSaaS, FILE_PREFIX, STATE_FILE_EXT } = require('../utils');
+const { requestSaaS, FILE_PREFIX } = require('../utils');
 const { Timings } = require('../lib/benchmark');
 
 async function getSkus(categoryPath, context) {
